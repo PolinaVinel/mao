97daeffc0f1e51add65be02990ea167fc9731f7b
polina <vinelpolina@icloud.com>
2018-10-21 15:03:05 +0500

    add D

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ data = pd.read_csv('data/mao_b.csv')
 # data.groupby(['time', 'concentration']).describe()
 # data['abs_c'] = 0
 
-data.loc[data.concentration == 5, 'abc_c'] = 0.2
-data.loc[data.concentration == 5, 'abc_c'] = 0.2
-data.loc[data.concentration == 2.5, 'abc_c'] = 0.1
-data.loc[data.concentration == 1.25, 'abc_c'] = 0.05
-data.loc[data.concentration == 0.625, 'abc_c'] = 0.025
+# data.loc[data.concentration == 5, 'abc_c'] = 0.2
+# data.loc[data.concentration == 5, 'abc_c'] = 0.2
+# data.loc[data.concentration == 2.5, 'abc_c'] = 0.1
+# data.loc[data.concentration == 1.25, 'abc_c'] = 0.05
+# data.loc[data.concentration == 0.625, 'abc_c'] = 0.025
+#
+# data['D'] = data.value / (data.time * data.abc_c)
+
+# save data
+# data.to_csv('data/mao_b.csv', index=False)
