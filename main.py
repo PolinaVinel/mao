d5677f0213fa1e12ff6edeb791d8e618ee0d997f
polina <vinelpolina@icloud.com>
2018-10-21 14:51:20 +0500

    add abs_c

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-import pandas
-import seaborn
+import pandas as pd
+import seaborn as sns
+
+data = pd.read_csv('data/mao_b.csv')
+
+# data.groupby(['time', 'concentration']).describe()
+# data['abs_c'] = 0
+
+data.loc[data.concentration == 5, 'abc_c'] = 0.2
+data.loc[data.concentration == 5, 'abc_c'] = 0.2
+data.loc[data.concentration == 2.5, 'abc_c'] = 0.1
+data.loc[data.concentration == 1.25, 'abc_c'] = 0.05
+data.loc[data.concentration == 0.625, 'abc_c'] = 0.025
