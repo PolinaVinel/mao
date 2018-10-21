5e9a1280fd965ff4618f80cfe6384de14fb69268
polina <vinelpolina@icloud.com>
2018-10-21 15:46:45 +0500

    end

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import seaborn as sns
 
+import matplotlib.pyplot as plt
+import matplotlib.transforms as transforms
+
 data = pd.read_csv('data/mao_b.csv')
+
 
 # data.groupby(['time', 'concentration']).describe()
 # data['abs_c'] = 0
@@ -16,3 +20,18 @@ data = pd.read_csv('data/mao_b.csv')
 
 # save data
 # data.to_csv('data/mao_b.csv', index=False)
+
+def show_plot(data, title):
+    fig, ax = plt.subplots()
+    # sns.swarmplot(x='concentration', y='D', data=data, edgecolor="black", linewidth=.9, ax=ax)
+    sns.boxplot(x='concentration', y='D', data=data, saturation=1, ax=ax)
+    sns.pointplot(x='concentration', y='D', data=data, linestyles='--', scale=0.4,
+                  color='k', errwidth=0, capsize=0, ax=ax)
+    plt.ylabel("D")
+    plt.title(title)
+
+    plt.show()
+
+
+# show_plot(data=data[data.time == 5], title='Time 5 min')
+show_plot(data=data[data.time == 10], title='Time 10 min')
