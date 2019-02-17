2cc7baf8bfca7c6b0fd046cc4c9424472fcd5a46
polina <vinelpolina@icloud.com>
2019-02-17 23:58:21 +0500

    change

--- main.py
+++ main.py
@@ -37,8 +37,8 @@ def show_concentration_plot(data, title):
 def show_d_plot(data, title):
     fig, ax = plt.subplots()
     # sns.swarmplot(x='concentration', y='D', data=data, edgecolor="black", linewidth=.9, ax=ax)
-    sns.boxplot(x='concentration', y='D', data=data, saturation=1, ax=ax)
-    sns.pointplot(x='concentration', y='D', data=data, linestyles='--', scale=0.4,
+    sns.boxplot(x='concentration', y='value', data=data, saturation=1, ax=ax)
+    sns.pointplot(x='concentration', y='value', data=data, linestyles='--', scale=0.4,
                   color='k', errwidth=0, capsize=0, ax=ax)
     plt.ylabel("D")
     plt.title(title)
@@ -51,8 +51,8 @@ def show_d_plot(data, title):
 def show_time_plot(data, title):
     fig, ax = plt.subplots()
     # sns.swarmplot(x='time', y='D', data=data, edgecolor="black", linewidth=.9, ax=ax)
-    sns.boxplot(x='time', y='D', data=data, saturation=1, ax=ax)
-    sns.pointplot(x='time', y='D', data=data, linestyles='--', scale=0.4,
+    sns.boxplot(x='time', y='value', data=data, saturation=1, ax=ax)
+    sns.pointplot(x='time', y='value', data=data, linestyles='--', scale=0.4,
                   color='k', errwidth=0, capsize=0, ax=ax)
     plt.ylabel("D")
     plt.title(title)
@@ -61,14 +61,50 @@ def show_time_plot(data, title):
     plt.savefig('plot/time_plot.svg', format='svg')
     plt.show()
 
-
-# show_d_plot(data=data[data.time == 15], title='Time 15 min')
-# show_d_plot(data=data[data.time == 20], title='Time 20 min')
-show_time_plot(data=data[data.concentration == 7.5], title='Concentration 7.5')
-show_time_plot(data=data[data.concentration == 3.75], title='Concentration 3.75')
-show_time_plot(data=data[data.concentration == 1.86], title='Concentration 1.86')
-show_time_plot(data=data[data.concentration == 0.93], title='Concentration 0.93')
-# show_concentration_plot(data=data, title='Time')
-# show_time_plot(data=data, title='concentration')
+#show_d_plot(data=data[data.time == 5], title='Время инкубации 5 мин')
+#show_d_plot(data=data[data.time == 10], title='Время инкубации 10 мин')
+#show_d_plot(data=data[data.time == 15], title='Время инкубации 15 мин')
+#show_d_plot(data=data[data.time == 20], title='Время инкубации 20 мин')
+#show_time_plot(data=data[data.concentration == 7.5], title='Концентрация белка 7.5 г/л')
+#show_time_plot(data=data[data.concentration == 3.75], title='Концентрация белка 3.75 г/л')
+#show_time_plot(data=data[data.concentration == 1.86], title='Концентрация белка 1.86 г/л')
+#show_time_plot(data=data[data.concentration == 0.93], title='Концентрация белка 0,93 г/л')
+#show_concentration_plot(data=data, title='Время,мин')
+#show_time_plot(data=data, title='Концентрация,г/л')
 
 data[(data.time == 15.0) & (data.concentration == 7.5)].value.describe()
+
+import pandas as pd
+import seaborn as sns
+
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+y = [0.15, .21, .39, .45, .51, .68, .74, .86, .98, 1.0]
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+#data = pd.read_csv('data/mao_b_r.csv')
+#d = data[data.time == 20]
+#t = d.groupby('concentration').value.mean()
+
+
+#x = np.array(t.index) data.
+#y = np.array(t.values)
+
+max = 0
+i_max = 0
+for i in range(0, len(x) - 1):
+    t = (y[i + 1] - y[i]) / (x[i + 1] - x[i])
+    if t > max:
+        max = t
+        i_max = i
+
+print(max)
+print('x1 = ', x[i_max])
+print('x2 = ', x[i_max + 1])
+print('y1 = ', y[i_max])
+print('y2 = ', y[i_max + 1])
+
+sns.lineplot(x, y)
+sns.lineplot([x[i_max], x[i_max + 1]], [y[i_max], y[i_max + 1]])
+plt.show()
